5b6a2ddafd2e4c0718cfba2ab77d892d870f2897
keerthibosoninov <keerthi@in.netroxe.com>
2019-09-17 10:15:53 +0530

    16-09-2019

---
+++ openemr1/sites/default/sqlconf.php
@@ -0,0 +1,38 @@
+<?php
+//  OpenEMR
+//  MySQL Config
+
+// $host	= 'localhost';
+// $port	= '3306';
+// $login	= 'openemr';
+// $pass	= 'p@ss123';
+// $dbase	= 'emr';
+
+
+$host	= 'localhost';
+$port	= '3306';
+$login	= 'openemr_test';
+$pass	= 'pass12345678';
+$dbase	= 'openemr';
+
+//Added ability to disable
+//utf8 encoding - bm 05-2009
+global $disable_utf8_flag;
+$disable_utf8_flag = false;
+
+$sqlconf = array();
+global $sqlconf;
+$sqlconf["host"]= $host;
+$sqlconf["port"] = $port;
+$sqlconf["login"] = $login;
+$sqlconf["pass"] = $pass;
+$sqlconf["dbase"] = $dbase;
+//////////////////////////
+//////////////////////////
+//////////////////////////
+//////DO NOT TOUCH THIS///
+$config = 1; /////////////
+//////////////////////////
+//////////////////////////
+//////////////////////////
+?>
